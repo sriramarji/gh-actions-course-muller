03f3ff0d1dd56e4055b5679ab655cbeabeee73ee
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-09-05 13:01:19 +0000

    feat(17-cust-actions): parse and validate inputs

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,51 @@
 const core = require('@actions/core');
- 
-async function run() { 
+const exec = require('@actions/exec');
+
+const validateBranchName = ({ branchName }) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const validateDirectoryName = ({ dirName }) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
+
+async function run() {
+  const baseBranch = core.getInput('base-branch');
+  const targetBranch = core.getInput('target-branch');
+  const ghToken = core.getInput('gh-token');
+  const workingDir = core.getInput('working-directory');
+  const debug = core.getBooleanInput('debug');
+
+
+  core.setSecret(ghToken);
+
+  if (!validateBranchName({ branchName: baseBranch })) {
+    core.error('Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes')
+    return;
+  }
+
+  if (!validateBranchName({ branchName: targetBranch })) {
+    core.error('Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes')
+    return;
+  }
+
+  if (!validateDirectoryName({ dirName: workingDir })) {
+    core.error('Invalid working-directory name. Directory names should include only characters, numbers, hyphens, underscores, dots, and forward slashes')
+    return;
+  }
+  
+  core.info(`[js-depedency-update]: base branch is ${baseBranch}`);
+  core.info(`[js-depedency-update]: target branch is ${targetBranch}`);
+  core.info(`[js-depedency-update]: working directory is ${workingDir}`);
+
+  await exec.exec('npm update', [], {
+    cwd: workingDir
+  });
+
+  const gitStatus = await exec.getExecOutput('git status -s package*.json', [], {
+    cwd: workingDir
+  })
+
+  if (gitStatus.stdout.length > 0) {
+    core.info('[js-depedency-update] : updates available!')
+  }else{
+    core.info('[js-depedency-update] : no updates at this point in time.')
+  }
   core.info('I am a custom JS action');
 }
  
